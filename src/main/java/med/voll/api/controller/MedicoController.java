1bd9a727ee82258000d94ccb3d72aa9069177f14
joaofm <joaofm@ciandt.com>
2023-01-06 13:41:48 -0300

    criação de cadastros de médico.

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,28 +1,38 @@
 package med.voll.api.controller;
 
 import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
+import med.voll.api.medico.DadosCadastroMedico;
 import med.voll.api.medico.Medico;
 import med.voll.api.medico.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import med.voll.api.medico.DadosCadastroMedico;
+import java.util.List;
 
 @RestController
 @RequestMapping("medicos")
 
-public class MedicoController {
+public class    MedicoController {
 
     @Autowired
     private MedicoRepository repository;
 
     @Transactional
     @PostMapping
-    public void cadastrar(@RequestBody DadosCadastroMedico dados) {
-        repository.save(new Medico(dados));
+    public void cadastrar(@RequestBody @Valid final DadosCadastroMedico dados) {
+        this.repository.save(new Medico(dados));
+    }
+
+    @GetMapping
+
+        public List<Medico> listar() {
+        return this.repository.findAll();
+
     }
 
 }
